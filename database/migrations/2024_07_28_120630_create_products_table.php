62cfc54928fe2c7483c19ea87269ae77fce3247d
HashimiNezhad <sayedrezahashiminezhad7@gmail.com>
2024-07-28 14:40:49 +0430

    commit

--- database/migrations/2024_07_28_120630_create_products_table.php
+++ database/migrations/2024_07_28_120630_create_products_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->integer('quantity');
             $table->decimal('price', 8, 2);
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
